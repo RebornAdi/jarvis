69fbedc9bf6a7a391b4f527988ee93e329c24e76
RebornAdi <145378359+RebornAdi@users.noreply.github.com>
2024-05-29 15:41:22 +0530

    Update functions.py

--- jarvis/functions.py
+++ jarvis/functions.py
@@ -24,35 +24,7 @@ def del_files(text):
     for i in range(1,100):
         os.rmdir(f"C:\\Users\\Aditya Atul Deshmukh\\Desktop\\test\\{i}")
 
-def open_vtop(username_text,password_text):
-    say("Opening vtop sir.")
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.support import expected_conditions as EC
-    import pyperclip
-    import time
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    browser.get('https://vtop.vit.ac.in/vtop/login')
-    xpath = '//*[@id="stdForm"]/a/div/div[2]/button'
-    student = WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    student.click()
-    user_xpath='//*[@id="username"]'
-    username=WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH,user_xpath)))
-    username.click()
-    pyperclip.copy(username_text)
-    username.send_keys(Keys.CONTROL,'v')
-    pass_xpath='//*[@id="password"]'
-    password=WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH,pass_xpath)))
-    password.click()
-    pyperclip.copy(password_text)
-    password.send_keys(Keys.CONTROL,'v')
-    vtop_xpath='//*[@id="b5-pagewrapper"]/div[1]/div/div/div/div[1]'
-    WebDriverWait(browser, 500).until(EC.presence_of_element_located((By.XPATH,vtop_xpath)))
-    time.sleep(5)
-    browser.quit()
+
 
 def whatsapp(text):
     from selenium import webdriver
